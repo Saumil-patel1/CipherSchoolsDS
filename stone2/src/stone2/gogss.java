fc846f340fcaa84adbe536c3a6ef86c9d2c34c53
Saumil <Saumil@codewithsaumil.com>
2020-05-28 13:41:12 +0530

    Revert "anagram"
    This reverts commit 0176f20a5db54ae6013e9b4cc2a826e69b54279a.

--- stone2/src/stone2/gogss.java
+++ stone2/src/stone2/gogss.java
@@ -2,33 +2,10 @@ package stone2;
 
 public class gogss {
 
+	public static void main(String[] args) {
+		int i=0;
+		System.out.println(i);
 
-		 	    static int Anagram(String str1, String str2) 
-	    { 
-	        
-	        int count1[] = new int[26];  
-	        int count2[] = new int[26]; 
-	  
-	        
-	        for (int i = 0; i < str1.length() ; i++) 
-	            count1[str1.charAt(i) -'a']++; 
-	      
-	      	        for (int i = 0; i < str2.length() ; i++) 
-	            count2[str2.charAt(i) -'a']++; 
-	  
-	       	        int result = 0; 
-	        for (int i = 0; i < 26; i++) 
-	            result += Math.abs(count1[i] - 
-	                               count2[i]); 
-	        return result; 
-	    } 
-	  
-	    	    public static void main(String[] args) 
-	    { 
-	        String str1 = "bcadeh", str2 = "hea"; 
-	        System.out.println(Anagram(str1, str2)); 
-	    } 
-	
 	}
 
-
+}
